69f69241e0c6a1428be7381befbf3654d8667873
Ksaveras Šakys <xawiers@gmail.com>
2020-02-24 22:37:19 +0000

    Update CS settings

--- tests/Storage/PsrCacheTest.php
+++ tests/Storage/PsrCacheTest.php
@@ -1,6 +1,12 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
+/*
+ * This file is part of ksaveras/circuit-breaker-bundle.
+ *
+ * (c) Ksaveras Sakys <xawiers@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Ksaveras\CircuitBreaker\Tests\Storage;
 
