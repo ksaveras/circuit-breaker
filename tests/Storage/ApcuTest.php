a42482c48698c7edbe699f9190a856a9b4a7a4b6
Ksaveras Sakys <xawiers@gmail.com>
2020-04-29 08:57:55 +0100

    CS fix

--- tests/Storage/ApcuTest.php
+++ tests/Storage/ApcuTest.php
@@ -1,4 +1,12 @@
 <?php declare(strict_types=1);
+/*
+ * This file is part of ksaveras/circuit-breaker-bundle.
+ *
+ * (c) Ksaveras Sakys <xawiers@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Ksaveras\CircuitBreaker\Tests\Storage;
 
@@ -23,7 +31,7 @@ class ApcuTest extends TestCase
 
     public function testSaveCircuit(): void
     {
-        if (!function_exists('apcu_store')) {
+        if (!\function_exists('apcu_store')) {
             $this->markTestSkipped();
         }
 
@@ -44,7 +52,7 @@ class ApcuTest extends TestCase
 
     public function testResetCircuit(): void
     {
-        if (!function_exists('apcu_store')) {
+        if (!\function_exists('apcu_store')) {
             $this->markTestSkipped();
         }
 
