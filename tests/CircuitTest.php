7d1df2c21af8e98f14dc7bd03581909fdf2e0b46
Ksaveras Sakys <xawiers@gmail.com>
2020-04-29 09:10:08 +0100

    CS fix

--- tests/CircuitTest.php
+++ tests/CircuitTest.php
@@ -7,7 +7,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace Ksaveras\CircuitBreaker\Tests;
 
 use Ksaveras\CircuitBreaker\Circuit;
