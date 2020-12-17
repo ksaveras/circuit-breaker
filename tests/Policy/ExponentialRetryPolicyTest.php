04e5511a16f0bda558c628689ba369e10de3111b
Ksaveras Sakys <xawiers@gmail.com>
2020-12-17 09:50:32 +0000

    Add missing headers

--- tests/Policy/ExponentialRetryPolicyTest.php
+++ tests/Policy/ExponentialRetryPolicyTest.php
@@ -1,9 +1,15 @@
 <?php declare(strict_types=1);
-
+/*
+ * This file is part of ksaveras/circuit-breaker.
+ *
+ * (c) Ksaveras Sakys <xawiers@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Ksaveras\CircuitBreaker\Tests\Policy;
 
 use Ksaveras\CircuitBreaker\Policy\ExponentialRetryPolicy;
-use Ksaveras\CircuitBreaker\Policy\LinearRetryPolicy;
 use Ksaveras\CircuitBreaker\Tests\Fixture\CircuitBuilder;
 use PHPUnit\Framework\TestCase;
 
--- tests/Policy/LinearRetryPolicyTest.php
+++ tests/Policy/LinearRetryPolicyTest.php
@@ -1,5 +1,12 @@
 <?php declare(strict_types=1);
-
+/*
+ * This file is part of ksaveras/circuit-breaker.
+ *
+ * (c) Ksaveras Sakys <xawiers@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Ksaveras\CircuitBreaker\Tests\Policy;
 
 use Ksaveras\CircuitBreaker\Policy\LinearRetryPolicy;
