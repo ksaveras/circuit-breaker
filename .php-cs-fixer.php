0de504bfcf2062eb38494a0178c1906736b709b7
Ksaveras Sakys <xawiers@gmail.com>
2021-06-23 07:47:55 +0100

    Upgrade CS fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -8,7 +8,13 @@ For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
 EOF;
 
-return PhpCsFixer\Config::create()
+$finder = PhpCsFixer\Finder::create()
+    ->in([__DIR__.'/src', __DIR__.'/tests'])
+;
+
+$config = new PhpCsFixer\Config();
+
+return $config
     ->setRiskyAllowed(true)
     ->setRules([
         '@Symfony' => true,
@@ -34,9 +40,6 @@ return PhpCsFixer\Config::create()
         'no_superfluous_phpdoc_tags' => ['allow_mixed' => true],
         'ordered_imports' => true,
         'php_unit_mock' => true,
+        'protected_to_private' => false,
     ])
-    ->setFinder(
-        PhpCsFixer\Finder::create()
-            ->files()
-            ->in([__DIR__.'/src', __DIR__.'/tests'])
-    );
+    ->setFinder($finder);
