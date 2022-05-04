b8b58ebfd83cc388aaec4b5ec7e9e9d5d4d0b903
Ksaveras Sakys <xawiers@gmail.com>
2022-05-04 19:20:49 +0100

    CS fix

--- src/CircuitBreakerFactory.php
+++ src/CircuitBreakerFactory.php
@@ -51,7 +51,7 @@ final class CircuitBreakerFactory
                     },
                 ]);
                 $policyResolver->setAllowedValues('type', ['constant', 'exponential', 'linear']);
-            }
+            },
         ]);
     }
 
